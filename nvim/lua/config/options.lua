8aa83fd8c11aa4c442fec91dc07074461055c6a4
dilokomi <baka@dilokomi.moe>
2026-07-09 14:50:39 +0800

    1

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,6 +5,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.tabstop = 4 -- 一个 Tab 显示为 4 个空格宽
 vim.opt.shiftwidth = 4 -- 自动缩进使用 4 个空格
 vim.opt.softtabstop = 4 -- 按 Tab/Backspace 时按 4 个空格处理
+vim.opt.expandtab = false -- 使用实际的 Tab 制表符而不是空格
 
 vim.opt.wrap = true
 vim.opt.linebreak = true
