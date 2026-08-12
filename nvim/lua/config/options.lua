a00653e113d9140616081f2a0c5cb78ca5b42560
dilokomi <baka@dilokomi.moe>
2026-08-12 10:50:55 +0800

    1

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.opt.clipboard = "unnamedplus"
+vim.opt.tabstop = 4 -- 一个 Tab 显示为 4 个空格宽
+vim.opt.shiftwidth = 4 -- 自动缩进使用 4 个空格
+vim.opt.softtabstop = 4 -- 按 Tab/Backspace 时按 4 个空格处理
+vim.opt.expandtab = false -- 使用实际的 Tab 制表符而不是空格
+
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
